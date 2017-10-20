70d5727bdce3b5aaff8c5dc5f6def8fb761ae8e7
Levanov Nikita <t.a.ross@yandex.com>
2017-10-20 20:45:21 +0300

    Pager Adapter

--- Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/view/MainActivity.java
+++ Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/view/MainActivity.java
@@ -1,15 +1,56 @@
 package ru.doubletapp.android.izjuminka.view;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
 import ru.doubletapp.android.izjuminka.R;
+import ru.doubletapp.android.izjuminka.view.profile.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_view_pager)
+    ViewPager mViewPager;
+
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewPager.setAdapter(new TabPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+        private static final int POSITION_NEWS = 0;
+        private static final int POSITION_PROFILE = 1;
+        private static final int POSITION_RATING = 2;
+
+        private static final int FRAGMENTS_COUNT = 3;
+
+        public TabPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case POSITION_PROFILE:
+                    return ProfileFragment.newInstance();
+                default:
+                    return new Fragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return FRAGMENTS_COUNT;
+        }
     }
 }

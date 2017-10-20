d7a87fbe1d93e78089f54945e22cec8235b21d1a
Denis Akimov <dakimov@doubletapp.ru>
2017-10-21 02:55:43 +0500

    #Fix wrong push

--- Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/view/login/LoginFragment.java
+++ Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/view/login/LoginFragment.java
@@ -1,5 +1,6 @@
 package ru.doubletapp.android.izjuminka.view.login;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;

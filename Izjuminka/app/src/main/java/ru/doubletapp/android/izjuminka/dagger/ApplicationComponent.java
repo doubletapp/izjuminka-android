197dd57c51333385602dbc16d10ddf3531f6b283
Levanov Nikita <t.a.ross@yandex.com>
2017-10-20 19:35:14 +0300

    Presenters Module

--- Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/dagger/ApplicationComponent.java
+++ Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/dagger/ApplicationComponent.java
@@ -1,15 +1,25 @@
 package ru.doubletapp.android.izjuminka.dagger;
 
+import android.support.annotation.NonNull;
+
 import javax.inject.Singleton;
 
 import dagger.Component;
+import ru.doubletapp.android.izjuminka.presenter.PresentersModule;
+import ru.doubletapp.android.izjuminka.presenter.profile.ProfilePresenter;
+import ru.doubletapp.android.izjuminka.view.profile.ProfileFragment;
 
 /**
  * Created by hash on 20/10/2017.
  */
 
 @Singleton
-@Component(modules = {AndroidApplicationModule.class, RetrofitModule.class})
+@Component(modules = {AndroidApplicationModule.class, RetrofitModule.class, PresentersModule.class})
 public interface ApplicationComponent {
+
+    @NonNull
+    ProfilePresenter getProfilePresenter();
+
+    void inject(ProfileFragment profileFragment);
 
 }
--- Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/view/profile/ProfileFragment.java
+++ Izjuminka/app/src/main/java/ru/doubletapp/android/izjuminka/view/profile/ProfileFragment.java
@@ -1,11 +1,47 @@
 package ru.doubletapp.android.izjuminka.view.profile;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import ru.doubletapp.android.izjuminka.IzjuminkaApplication;
+import ru.doubletapp.android.izjuminka.R;
+import ru.doubletapp.android.izjuminka.presenter.profile.ProfilePresenter;
+import ru.doubletapp.android.izjuminka.view.BaseFragment;
+
 /**
  * Created by hash on 20/10/2017.
  */
 
-public class ProfileFragment {
+public class ProfileFragment extends BaseFragment<ProfilePresenter> {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        IzjuminkaApplication.getApplicationComponent(getContext()).inject(this);
+        super.onCreate(savedInstanceState);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+        initialize();
+        return view;
+    }
 
+    private void initialize() {
+        // TODO: add some init
+    }
+
+    @Nullable
+    @Override
+    protected ProfilePresenter createPresenter() {
+        return IzjuminkaApplication
+                .getApplicationComponent(getContext())
+                .getProfilePresenter();
+    }
 }
